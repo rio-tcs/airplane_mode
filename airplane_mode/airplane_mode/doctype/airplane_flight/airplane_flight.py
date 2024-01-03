66a50c484949f7c87e499173aa532d3fc15984fe
rio-tcs <riopramana@tobaconsulting.com>
2024-01-03 17:06:33 +0700

    Added logic for gate number in Flight & Ticket, added Crew Members to Flight

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -1,10 +1,24 @@
 # Copyright (c) 2023, Rio Pramana and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class AirplaneFlight(WebsiteGenerator):
     def on_submit(self):
-        self.status = 'Completed'
+        if not self.gate_number:
+            frappe.throw("A gate number must be assigned before submitting")
+        self.status = "Completed"
+        # update every ticket's gate number
+        tickets = frappe.get_all(
+            "Airplane Ticket",
+            filters={"flight": self.name},
+            fields=["name", "gate_number"],
+        )
+        for ticket in tickets:
+            ticket_doc = frappe.get_doc("Airplane Ticket", ticket.name)
+            ticket_doc.gate_number = self.gate_number
+            ticket_doc.save()
+
+        frappe.db.commit()
