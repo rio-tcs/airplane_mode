65ee3ec93ba1a48dd217118054270111955541d3
rio-tcs <riopramana@tobaconsulting.com>
2024-01-04 16:56:24 +0700

    Added physical properties to Shop and made print format for Rent Receipt

--- airplane_mode/airport_shop/doctype/shop/shop.py
+++ airplane_mode/airport_shop/doctype/shop/shop.py
@@ -10,8 +10,10 @@ class Shop(Document):
         if self.available_for_lease:
             if self.lease_fee_per_month <= 0:
                 frappe.throw("Lease fee cannot be zero")
-                
+
             if frappe.db.exists(
                 {"doctype": "Shop Lease Contract", "shop": self.name, "docstatus": 1}
             ):
                 frappe.throw("An active contract for this Shop already exists")
+        if self.length and self.width:
+            self.area_m2 = self.length * self.width
